838521b68111b1b86b9b66221fb5c9a1760e579d
Samsudeen Ashad <samsudeenashad@icloud.com>
2026-01-02 01:07:44 +0530

    update favicons

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -54,6 +54,11 @@ export default function RootLayout({
   return (
     <html lang="en" suppressHydrationWarning>
       <head>
+        {/* Favicon for Google Search and browsers */}
+        <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" />
+        <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png" />
+        <link rel="icon" href="/favicon.ico" sizes="48x48" />
+        <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png" />
         <link rel="icon" href="/favicon.svg" type="image/svg+xml" />
         <script
           dangerouslySetInnerHTML={{
